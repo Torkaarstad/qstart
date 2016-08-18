13af45d7f8ea722a7b1a340f8cdeb2a3ed102194
Tor Kårstad <tor.kaarstad@unimicro.no>
2016-08-18 17:22:11 +0200

    Copy from staringline

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-﻿"use strict";
+"use strict";
 var gulp = require("gulp");
 var del = require("del");
 var sourcemaps = require('gulp-sourcemaps');
@@ -13,12 +13,27 @@ gulp.task('clean', function (cb) {
 /**
  * Copy all resources that are not TypeScript files into build directory.
  */
-gulp.task("resources", ["server"], function () {
-    return gulp.src(["src/**/*", "!**/*.ts", "!src/server", "!src/server/**"])
+gulp.task("resources", ["server", "app", "assets"], function () {
+    console.log("Building resources...");
+});
+/* copy the app core files to the build folder */
+gulp.task("app", ['index'], function(){
+    return gulp.src(["app/**", "!app/**/*.ts"])
+        .pipe(gulp.dest("build/app"));
+});
+/* get the index file to the root of the build */
+gulp.task("index", function(){
+    return gulp.src(["index.html"])
         .pipe(gulp.dest("build"));
 });
+/* copy node server to build folder */
 gulp.task("server", function () {
-    return gulp.src(["index.js", "package.json"], { cwd: "src/server/**" })
+    return gulp.src(["index.js", "package.json"], { cwd: "server/**" })
+        .pipe(gulp.dest("build"));
+});
+/* styles and other assets */
+gulp.task("assets", function(){
+    return gulp.src(["styles.css"])
         .pipe(gulp.dest("build"));
 });
 /**
@@ -35,7 +50,7 @@ gulp.task("libs", function () {
         'angular2/bundles/angular2.dev.js',
         'angular2/bundles/router.dev.js'
     ], { cwd: "node_modules/**" }) /* Glob required here. */
-        .pipe(gulp.dest("build/lib"));
+        .pipe(gulp.dest("build/node_modules"));
 });
 /**
  * Build the project.
